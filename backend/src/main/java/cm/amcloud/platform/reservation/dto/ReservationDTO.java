be70af90a7b251d573982d0e7b8116a1f421907e
mnanfack <mnanfack@irex.aretex.ca>
2025-06-03 05:13:32 +0100

    changed the iam logic & enabled ssl communication by adding nginx reverse proxy.

--- backend/src/main/java/cm/amcloud/platform/reservation/dto/ReservationDTO.java
+++ backend/src/main/java/cm/amcloud/platform/reservation/dto/ReservationDTO.java
@@ -1,20 +1,21 @@
 package cm.amcloud.platform.reservation.dto;
 
+import lombok.AllArgsConstructor; 
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor; 
 import lombok.Setter;
 
-@Data
-@Getter
-@Setter
+@Data 
+@Getter 
+@Setter 
+@NoArgsConstructor 
+@AllArgsConstructor 
 public class ReservationDTO {
 
     private String travelerName;
     private String destination;
     private String agencyName;
-    public ReservationDTO(String travelerName, String destination, String agencyName) {
-        this.travelerName = travelerName;
-        this.destination = destination;
-        this.agencyName = agencyName;
-    }
+
+     
 }
